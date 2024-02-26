4f548fc059d25ba44a575267123faec8af29441e
krish <championcoc.212@gmail.com>
2024-02-26 11:33:21 +0530

    fix

--- main.py
+++ main.py
@@ -138,5 +138,4 @@ async def generate_material(url: Url):
     return parsed_responses
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
